9716bf4e887b97797639f3b92f6ceb9895913268
uflowie <bananasawsm@gmail.com>
2025-10-08 23:54:16 +0200

    set up sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>flowie2k</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <div className="p-2">Hello from the sidebar!</div>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href={"google.com"}>
+                    <span>Google</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
--- src/react-app/routes/__root.tsx
+++ src/react-app/routes/__root.tsx
@@ -1,9 +1,13 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 const RootLayout = () => (
-  <>
+  <SidebarProvider>
+    <AppSidebar />
     <div className="p-2 flex gap-2">
+      <SidebarTrigger />
       <Link to="/" className="[&.active]:font-bold">
         Home
       </Link>{' '}
@@ -14,7 +18,7 @@ const RootLayout = () => (
     <hr />
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </SidebarProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
